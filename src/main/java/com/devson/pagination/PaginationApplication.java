331f72a7f5ca9fe1f0427085461aec65152c5d7f
devson <sonyc5720@gmail.com>
2019-01-01 15:27:19 +0900

    게시물 페이징을 위해 디폴트 데이터를 추가하는 CommandLineRunner 추가

--- src/main/java/com/devson/pagination/PaginationApplication.java
+++ src/main/java/com/devson/pagination/PaginationApplication.java
@@ -1,7 +1,15 @@
 package com.devson.pagination;
 
+import com.devson.pagination.web.domain.BoardEntity;
+import com.devson.pagination.web.domain.UserEntity;
+import com.devson.pagination.web.repository.BoardRepository;
+import com.devson.pagination.web.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class PaginationApplication {
@@ -10,5 +18,24 @@ public class PaginationApplication {
         SpringApplication.run(PaginationApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(UserRepository userRepository, BoardRepository boardRepository) {
+        return args -> {
+            IntStream.rangeClosed(1, 200).forEach(i -> {
+                UserEntity user =  UserEntity.builder()
+                        .name("tester" + i)
+                        .build();
+
+                userRepository.save(user);
+
+                BoardEntity board = BoardEntity.builder()
+                        .title("test" + i)
+                        .writer(user)
+                        .build();
+
+                boardRepository.save(board);
+            });
+        };
+    }
 }
 
