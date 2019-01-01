a906fe2cc634ab7393c26b17442283f84922fe6c
devson <sonyc5720@gmail.com>
2019-01-01 15:28:13 +0900

    boardView 메서드에 Pageable, Model 인자 추가

--- src/main/java/com/devson/pagination/web/controller/BoardController.java
+++ src/main/java/com/devson/pagination/web/controller/BoardController.java
@@ -1,13 +1,16 @@
 package com.devson.pagination.web.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
     @GetMapping("/boards")
-    public String boardView() {
+    public String boardView(Model model, @PageableDefault Pageable pageable) {
         return "board";
     }
 }
