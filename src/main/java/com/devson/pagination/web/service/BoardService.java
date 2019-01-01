ece58de2cf157fdd55bc05885c77621ad6ab4753
devson <sonyc5720@gmail.com>
2019-01-01 15:39:30 +0900

    Pageable을 이용한 현재 페이지의 게시물 가져오기

---
+++ src/main/java/com/devson/pagination/web/service/BoardService.java
@@ -0,0 +1,26 @@
+package com.devson.pagination.web.service;
+
+import com.devson.pagination.web.domain.BoardEntity;
+import com.devson.pagination.web.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+    private static final int PAGE_SIZE = 10;
+
+    private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Page<BoardEntity> getBoard(Pageable pageable) {
+        int pageNumber = (pageable.getPageNumber() == 0) ? 0 : pageable.getPageNumber() - 1;
+        pageable = PageRequest.of(pageNumber, PAGE_SIZE);
+
+        return boardRepository.findAll(pageable);
+    }
+}
