02fe3a96f6af17059ce7f25c0bc65d8b805c61b1
devson <sonyc5720@gmail.com>
2019-01-11 21:28:40 +0900

    Entity 생성

---
+++ src/main/java/com/devson/pagination/web/domain/BoardEntity.java
@@ -0,0 +1,27 @@
+package com.devson.pagination.web.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "BOARD") // 없을 경우 BOARD_ENTITY로 생성됨
+public class BoardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private UserEntity writer;
+
+    @Builder
+    public BoardEntity(String title, UserEntity writer) {
+        this.title = title;
+        this.writer = writer;
+    }
+}
