bba29b495badb0875622caa9c5c2a64a19d1502c
devson <sonyc5720@gmail.com>
2019-01-11 23:52:22 +0900

    service 생성

---
+++ src/main/java/com/devson/pagination/web/service/BoardService.java
@@ -0,0 +1,24 @@
+package com.devson.pagination.web.service;
+
+import com.devson.pagination.web.domain.BoardEntity;
+import com.devson.pagination.web.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Page<BoardEntity> getBoardList(Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10);
+        
+        return boardRepository.findAll(pageable);
+    }
+}
