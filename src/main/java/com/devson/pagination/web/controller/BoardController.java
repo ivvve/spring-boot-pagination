b449052e0a469accc9898d3bba1d3aa26f736864
devson <sonyc5720@gmail.com>
2019-01-12 00:14:31 +0900

    Controller 로직 추가

--- src/main/java/com/devson/pagination/web/controller/BoardController.java
+++ src/main/java/com/devson/pagination/web/controller/BoardController.java
@@ -1,15 +1,33 @@
 package com.devson.pagination.web.controller;
 
+import com.devson.pagination.web.domain.BoardEntity;
+import com.devson.pagination.web.service.BoardService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class BoardController {
+    private BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
 
     @GetMapping("/boards")
-    public String boardView(@PageableDefault Pageable pageable) {
+    public String boardView(@PageableDefault Pageable pageable, Model model) {
+        Page<BoardEntity> boardList = boardService.getBoardList(pageable);
+        model.addAttribute("boardList", boardList);
+
+        log.debug("총 element 수 : {}, 전체 page 수 : {}, 페이지에 표시할 element 수 : {}, 현재 페이지 index : {}, 현재 페이지의 element 수 : {}",
+                boardList.getTotalElements(), boardList.getTotalPages(), boardList.getSize(),
+                boardList.getNumber(), boardList.getNumberOfElements());
+
         return "board";
     }
 }
