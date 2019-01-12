be82d8f38085d6ac04cbfa8956d224da5a7ffc64
devson <sonyc5720@gmail.com>
2019-01-12 23:59:59 +0900

    sort 추가

--- src/main/java/com/devson/pagination/web/service/BoardService.java
+++ src/main/java/com/devson/pagination/web/service/BoardService.java
@@ -5,6 +5,7 @@ import com.devson.pagination.web.repository.BoardRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,8 +18,8 @@ public class BoardService {
 
     public Page<BoardEntity> getBoardList(Pageable pageable) {
         int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
-        pageable = PageRequest.of(page, 10);
-        
+        pageable = PageRequest.of(page, 10, new Sort(Sort.Direction.DESC, "id"));
+
         return boardRepository.findAll(pageable);
     }
 }
